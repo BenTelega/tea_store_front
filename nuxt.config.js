0b82c5d4bc79ad018579ed09730275433df22568
BenTelega <benjamin.chebotarev@ya.ru>
2023-11-01 21:19:00 +0300

    fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -56,7 +56,9 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'https://2108172431.ru',
+    baseURL: 'https://shoppify-test.ru',
+    // baseURL: 'http://localhost:8000',
+    // browserBaseURL: 'http://localhost:8000',
   },
 
   auth: {
